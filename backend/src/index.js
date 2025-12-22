921021ac17f1fdfb3dc648f9b8c0806c36ed3f37
dimonrtm <dimonrtm@gmail.com>
2025-12-22 20:22:10 +0500

    feat: add routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,33 @@
 const http = require("http")
 
 const server = http.createServer((req, res) =>{
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain; charset=utf-8");
-	res.end("Hello from server")
+	const host = req.headers.host ?? "localhost";
+	const url = new URL(req.url ?? "/", `http://${host}`);
+	const path = url.pathname;
+	if(req.method !== "GET"){
+		res.statusCode = 405;
+		res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.end("Method Not Allowed");
+        return;
+	}
+
+	if(path === "/"){
+		res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain; charset=utf-8");
+        res.end("Hello");
+        return;
+	}
+
+	if (path === "/health") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.end("OK");
+    return;
+  }
+
+  res.statusCode = 404;
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.end("Not Found");
 });
 
 server.listen(3000, "0.0.0.0", () =>{
