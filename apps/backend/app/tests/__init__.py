9ff3f64693a000a69d0d43640995d84b4a2dcbc4
dimonrtm <dimonrtm@gmail.com>
2026-01-07 21:17:24 +0500

    fix: add packeges

---
+++ apps/backend/app/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  7 21:11:42 2026
+
+@author: dimon
+"""
---
+++ apps/backend/app/tests/test_sanity.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  7 21:12:17 2026
+
+@author: dimon
+"""
+
+
+def test_sanity() -> None:
+    assert 1 + 1 == 2
