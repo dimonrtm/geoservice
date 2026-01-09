5fb45664053d20f42e3bb4580611415559d952bd
dimonrtm <dimonrtm@gmail.com>
2026-01-09 18:10:53 +0500

    feat: add cors middleware

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -10,20 +10,19 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.lifespan import lifespan
 from api.auth import auth_router
 from api.secure_router import secure_router
+import os
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:5173,http://127.0.0.1:5173")
+origins = CORS_ORIGINS.split(",")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 app.include_router(auth_router)
