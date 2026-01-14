784ed7c85194417eb8fc51c167f9c58854a8ceed
dimonrtm <dimonrtm@gmail.com>
2026-01-14 19:34:19 +0500

    fix: pretier

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import CheckAuthComponent from "./components/CheckAuthComponent.vue";
 import MapView from "./components/MapView.vue";
-import "./assets/main.css"
+import "./assets/main.css";
 </script>
 
 <template>
   <CheckAuthComponent />
-  <MapView/>
+  <MapView />
 </template>
 
 <style scoped>
-.page { height: 100%; display: flex; flex-direction: column; }
-.content { flex: 1; min-height: 0; } /* важно для flex-раскладки */
+.page {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.content {
+  flex: 1;
+  min-height: 0;
+} /* важно для flex-раскладки */
 </style>
--- apps/frontend/src/components/MapView.vue
+++ apps/frontend/src/components/MapView.vue
@@ -1,57 +1,57 @@
 <template>
-    <div ref="mapEl" class="mapEl"></div>
+  <div ref="mapEl" class="mapEl"></div>
 </template>
 
 <script setup lang="ts">
-   import {ref, onMounted, onBeforeUnmount, shallowRef} from "vue";
-   import {Map, NavigationControl, type StyleSpecification} from "maplibre-gl";
-   import "maplibre-gl/dist/maplibre-gl.css"
+import { ref, onMounted, onBeforeUnmount, shallowRef } from "vue";
+import { Map, NavigationControl, type StyleSpecification } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 
-   const mapEl = ref<HTMLDivElement | null>(null);
-   const map = shallowRef<Map |null>(null);
-   const style: StyleSpecification = {
-    version: 8,
-    sources: {
-        osm: {
-            type: "raster",
-            tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-            tileSize: 256,
-             attribution: "© OpenStreetMap contributors",
-        },
+const mapEl = ref<HTMLDivElement | null>(null);
+const map = shallowRef<Map | null>(null);
+const style: StyleSpecification = {
+  version: 8,
+  sources: {
+    osm: {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+      tileSize: 256,
+      attribution: "© OpenStreetMap contributors",
     },
-    layers: [
-        {
-            id: "osm",
-            type: "raster",
-            source: "osm",
-        },
-    ],
-   };
+  },
+  layers: [
+    {
+      id: "osm",
+      type: "raster",
+      source: "osm",
+    },
+  ],
+};
 
-   onMounted(() => {
-    if(!mapEl.value){
-        return;
-    }
-     map.value = new Map({
-        container: mapEl.value,
-         style,
-          center: [70.1902, 52.937],
-           zoom: 3
-        });
-    map.value.addControl(new NavigationControl(), "top-right");
-     console.log("Карта создана");
-   })
+onMounted(() => {
+  if (!mapEl.value) {
+    return;
+  }
+  map.value = new Map({
+    container: mapEl.value,
+    style,
+    center: [70.1902, 52.937],
+    zoom: 3,
+  });
+  map.value.addControl(new NavigationControl(), "top-right");
+  console.log("Карта создана");
+});
 
-   onBeforeUnmount(() => {
-    map.value?.remove();
-    map.value = null;
-    console.log("Карта удалена");
-   })
+onBeforeUnmount(() => {
+  map.value?.remove();
+  map.value = null;
+  console.log("Карта удалена");
+});
 </script>
 
 <style scoped>
-    .mapEl{
-        width: 100%;
-        height: 100%;
-    }
+.mapEl {
+  width: 100%;
+  height: 100%;
+}
 </style>
